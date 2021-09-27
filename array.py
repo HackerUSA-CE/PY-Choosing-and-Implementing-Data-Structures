fd437dc4afda220bf6ad7db76d2351f1f17c52b8
nkleinmann <nekleinmann@gmail.com>
2021-09-27 13:00:10 +0200

    Update solutions

--- array.py
+++ array.py
@@ -1,1 +1,43 @@
 # Write code for array challenges below
+
+import numpy as np
+
+# Question 1
+a = np.array([1,2,3,4])
+
+def show_elements(arr):
+    for el in arr:
+        print(el)
+
+show_elements(a)
+
+# End of Question 1 Solution
+
+# Question 2 - there are several ways to accomplist this
+
+b = np.array([1,5,-2,10])
+
+# iterative approach
+def array_sum(numberArr):
+    arrSum = 0
+    for i in numberArr:
+        arrSum = arrSum + i
+    return arrSum
+
+print(array_sum(b))
+
+# recursive approach
+def array_sum_recursive(numberArr):
+    if len(numberArr) == 1:
+        return numberArr[0]
+    
+    # Recursive case
+    return numberArr[0] + array_sum(numberArr[1:])
+
+print(array_sum_recursive(b))
+
+
+# using NumPy sum operator
+print(b.sum())
+
+# End of Question 2 Solution
--- linked_list.py
+++ linked_list.py
@@ -1,1 +1,39 @@
 # Write code for implementing a linked list below
+
+class Node:
+    def __init__(self, data, reference=None):
+        self.data = data
+        self.reference = reference
+
+# node1 = Node(5)
+# print(node1.data)
+# node2=Node(11)
+# node1.reference=node2
+# print(node1.reference)
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_linked_list(self):
+        if self.head == None:
+            print("The linked list is empty")
+        else:
+            c_node = self.head
+            while c_node is not None:
+                print(c_node.data)
+                c_node = c_node.reference
+
+    def add_to_start(self, data):
+        n_node = Node(data)
+        n_node.reference = self.head
+        self.head = n_node
+            
+# linked_list1 = LinkedList()
+# linked_list1.print_linked_list()
+
+linked_list1 = LinkedList()
+linked_list1.add_to_start(82)
+linked_list1.add_to_start(15)
+linked_list1.print_linked_list()
