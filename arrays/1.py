bddadb1b1d2b83faad8545b80962170245f4f1bd
maepena91 <maepena91@yahoo.com>
2022-07-05 11:32:57 -0700

    finished activity

--- arrays/1.py
+++ arrays/1.py
@@ -5,8 +5,8 @@ import numpy as np
 a = np.array([1,2,3,4])
 
 def list(a):
-    for element in a:
-        print(element)
+    for arr in a:
+        print(arr)
 
 list(a)
 
--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+array = [1,6,3] 
+
+array_arr = np.array(array)
+np.multiply = array_arr * 3
+
+print(np.multiply)
